6aa295f5141a2e7d9199683ca561afb9f76307cb
Nikhil Sinha <n.sinha1982@gmail.com>
2021-08-22 21:26:23 -0400

    car controller created

--- force-app/main/default/lwc/carTileList/carTileList.js
+++ force-app/main/default/lwc/carTileList/carTileList.js
@@ -1,3 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getCars from '@salesforce/apex/CarController.getCars'
+export default class CarTileList extends LightningElement {
 
-export default class CarTileList extends LightningElement {}
+    cars
+    error
+    @wire(getCars)
+    carsHandler({data, error}){
+        if(data){
+            console.log(data)
+            this.cars = data
+        }
+        if(error){
+            console.error(error)
+            this.error = error
+        }
+    }
+}
